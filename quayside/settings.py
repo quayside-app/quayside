328f2ee2ea5d530ef20949a3383b7882fa846ae0
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-02-04 02:48:29 -0900

    Added readme

--- quayside/settings.py
+++ quayside/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "app",
     "api",
     "compressor",  # For tailwind
+    "django_browser_reload",  # For automatic reload
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",  # For automatic reload
 ]
 
 ROOT_URLCONF = "quayside.urls"
