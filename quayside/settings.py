39b49185c8f9359d02799ca00dfc59da58be30a2
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-02-04 02:22:56 -0900

    Tailwind fully working and happy now

--- quayside/settings.py
+++ quayside/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -124,7 +124,11 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # For tailwind
-COMPRESS_ROOT = BASE_DIR / 'app' / 'static'
+COMPRESS_ROOT = BASE_DIR / 'staticfiles'
 COMPRESS_ENABLED = True
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+]
 
--- quayside/urls.py
+++ quayside/urls.py
@@ -18,7 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from .views import index
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', index, name='index')
-]
+    path('', index, name='index'),
+] 
+
+
+
