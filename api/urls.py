7c7322b6672ee9f78ce42f814b516d40891bde24
kaiverson <mwbamber@alaska.edu>
2024-02-09 16:16:12 -0900

    added basic endpoint info

--- api/urls.py
+++ api/urls.py
@@ -5,3 +5,14 @@ from . import views
 urlpatterns = [
     path("/users/:username", views.index, name="index"),
 ]
+
+# GET /api/v1/users/:username                   Get information about a user
+#           no req
+#           res = {email:String, username:String, firstName:String, lastName:String}
+
+
+
+# GET /api/v1/projects?uid={}&projectID={}      Get a some form of a list of projects
+# GET /api/v1/project/:projectID                Get an individual project
+# GET /api/v1/search                            Search for something at some point; do later
+# GET /api/v1/project/:projectID/tasks
