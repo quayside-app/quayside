a982fb1d07e14c7cc87581b5ea3a3ba8c6dc67ec
UnderYourSpell <mwbamber@alaska.edu>
2024-02-09 15:43:33 -0900

    Added Project model

--- api/models.py
+++ api/models.py
@@ -10,5 +10,33 @@ class User(mongo.Document):
     meta = {
         'collection': 'User', # Need to specify UPPER Case
         'strict': False  # If true, throws weird error for __v
-        }  
+        } 
+
+class Project(mongo.Document):
+    name = mongo.StringField()
+    types = mongo.ListField(mongo.StringField())
+    objectives = mongo.ListField(mongo.StringField())
+    startDate = mongo.DateField()
+    endDate = mongo.DateField()
+    budget = mongo.StringField()
+    assumptions = mongo.ListField(mongo.StringField())
+    scopesIncluded = mongo.ListField(mongo.StringField())
+    scopesExcluded = mongo.ListField(mongo.StringField())
+    risks = mongo.ListField(mongo.StringField())
+    userIDs = mongo.ListField(mongo.ObjectIdField())
+    projectManagerIDs = mongo.ListField(mongo.ObjectIdField())
+    sponsors = mongo.ListField(mongo.StringField())
+    contributerIDs = mongo.ListField(mongo.ObjectIdField())
+    completionRequirements = mongo.ListField(mongo.StringField())
+    qualityAssurance = mongo.ListField(mongo.StringField())
+    KPIs = mongo.ListField(mongo.StringField())
+    otherProjectDepencies = mongo.ListField(mongo.ObjectIdFieldField())
+    informationLinks = mongo.ListField(mongo.StringField())
+    completionStatus = mongo.StringField()
+    teams = mongo.ListField(mongo.ObjectIdField())
+    meta = {
+        'collection': 'Project', # Need to specify UPPER Case
+        'strict': False  # If true, throws weird error for __v
+        } 
+
 
