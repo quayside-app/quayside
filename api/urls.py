4229483c6025c249750c02ebb71ef2d855537f64
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-02-11 20:03:47 -0900

    Project route working

--- api/urls.py
+++ api/urls.py
@@ -5,11 +5,20 @@ API_VERSION = "v1"
 views = importlib.import_module(name=f".views.{API_VERSION}", package="api")
 
 
+
 urlpatterns = [
-    path(f"{API_VERSION}/users/<str:username>/",
-         views.users.UserDetailAPIView.as_view(), name="v1-user-detail"),
-    path(f"{API_VERSION}/projects/",
-         views.projects.ProjectsAPIView.as_view(), name="v1-projects"),
+     path(f"{API_VERSION}/user/<str:username>/",
+          views.user.UserDetailAPIView.as_view(), name="v1-user-details"),
+
+     path(f"{API_VERSION}/projects/",
+          views.projects.ProjectsAPIView.as_view(), name="v1-projects-list"),
+     
+     path(f"{API_VERSION}/project/<str:id>/",
+          views.project.ProjectAPIView.as_view(), name="v1-project-details"),
+
+
+     # path(f"{API_VERSION}/project/<str:projectID>/", 
+     #      views.project.ProjectAPIView.as_view(), name='v1-project-details'),
 ]
 
 # GET /api/v1/users/:username                   Get information about a user
