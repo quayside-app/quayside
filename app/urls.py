a0aac399d39ffb3bda07c08fb216a132ce4809ef
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-02-13 12:13:07 -0900

    Added CreateProjectModal view and modal

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('create-project', views.create_project, name='create_project_view'),
     path('project/<str:projectID>/', views.project, name='project_view'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def project(request, projectID):
     return render(request, 'project.html', {'project_ID': projectID})
+
+def create_project(request):
+    return render(request, 'createProjectModal.html', )
