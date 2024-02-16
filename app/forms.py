db05b6260c8c1ed2416f7455f89d9004c3258462
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-02-15 20:08:41 -0900

    Task modal working finally

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,7 @@ class NewProjectForm(forms.Form):
     description = forms.CharField(label="What is your project about?",
                                   widget=forms.TextInput(attrs={
                                       "placeholder": "I want to bake a cake!",
-                                      "class": "w-96 block mt-3 text-sm rounded-md mt-1 p-2 bg-neutral-600 outline-none placeholder-gray-400",
+                                      "class": "w-96 block mt-3 p-2 text-sm rounded-md   bg-neutral-600 outline-none placeholder-gray-400",
                                       "type": "text",
                                   })
                                   )
@@ -15,14 +15,18 @@ class NewProjectForm(forms.Form):
 
 class TaskForm(forms.Form):
     # Can't use forms.ModelForm bc it's mongo
-    name = forms.CharField(label="Name",
+    name = forms.CharField(label="",
                            widget=forms.TextInput(attrs={
                                "type": "text",
+                               "class": "w-full block bg-neutral-800 outline-none sm:text-2xl font-bold",
                            })
                            )
 
-    description = forms.CharField(label="Description",
-                                  widget=forms.TextInput(attrs={
+    description = forms.CharField(label="",
+                                  required=False,
+                                  widget=forms.Textarea(attrs={
+                                      "placeholder": "Description...",
                                       "type": "text",
+                                      "class": "w-full block p-2 mt-4 rounded-md  bg-neutral-600 outline-none placeholder-gray-400",
                                   })
                                   )
--- app/views.py
+++ app/views.py
@@ -4,30 +4,49 @@ from django.http import HttpResponseRedirect
 from .forms import NewProjectForm, TaskForm
 from api.views.v1.generatedTasks import GeneratedTasks
 from api.views.v1.projects import ProjectsAPIView
+from api.views.v1.tasks import TasksAPIView
 
 
 
 def projectGraphView(request, projectID):
-    return render(request, 'graph.html', {'project_ID': projectID})
+    return render(request, "graph.html", {"project_ID": projectID})
 
 def taskView(request, projectID, taskID):
+    print("IN CREATE TASK VEW")
     if request.method == "POST":
         form = TaskForm(request.POST)
 
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            #return HttpResponseRedirect("/thanks/")
-            pass
+            newData = form.cleaned_data
+            newData["id"] = taskID
+            print(newData)
 
-    # if a GET (or any other method) we'll create a blank form
+            data, code = TasksAPIView.updateTask(newData)
+
+            print("UPDATED TASKS...")
+
+            print(data)
+            print(code)
+        
+
+    # if a GET (or any other method) we"ll create a blank form
     else:
-        form = TaskForm()
-    return render(request, 'taskModal.html', {'project_ID': projectID, 'taskID': taskID, "form": form})
+        print("HEREEEE")
+        taskData = TasksAPIView.getTasks({"id": taskID})[0][0]
+        # Populate initial form data
+        if taskData is not None:
+            initialData = {
+                "name": taskData.get("name", ""),
+                "description": taskData.get("description", ""),
+            }
+            form = TaskForm(initial=initialData)
+        else:
+            form = TaskForm()
+    return render(request, "taskModal.html", {"project_ID": projectID, "taskID": taskID, "form": form})
 
 
 def createProjectView(request):
+    
     # If this is a POST request, process the form data
     if request.method == "POST":
         form = NewProjectForm(request.POST)
