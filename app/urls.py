07a23c9b618ca2a03f8f17cf8493db99e0f4aea4
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-02-15 18:14:32 -0900

    Edit task modal pulling up on click

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create-project', views.createProject, name='create-project-view'),
-    path('project/<str:projectID>/graph', views.projectGraph, name='project-graph-view'),
+    path('create-project', views.createProjectView, name='create-project-view'),
+    path('project/<str:projectID>/graph', views.projectGraphView, name='project-graph-view'),
+    path('project/<str:projectID>/graph/task/<str:taskID>', views.taskView, name='task-detail-view'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,17 +1,33 @@
 
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .forms import NewProjectForm
+from .forms import NewProjectForm, TaskForm
 from api.views.v1.generatedTasks import GeneratedTasks
 from api.views.v1.projects import ProjectsAPIView
 
 
 
-def projectGraph(request, projectID):
+def projectGraphView(request, projectID):
     return render(request, 'graph.html', {'project_ID': projectID})
 
+def taskView(request, projectID, taskID):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
 
-def createProject(request):
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            #return HttpResponseRedirect("/thanks/")
+            pass
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = TaskForm()
+    return render(request, 'taskModal.html', {'project_ID': projectID, 'taskID': taskID, "form": form})
+
+
+def createProjectView(request):
     # If this is a POST request, process the form data
     if request.method == "POST":
         form = NewProjectForm(request.POST)
