c549b8cecddcc86721d9fe7a2bdab611ea355f78
Kai Iverson <kaiiverson510@gmail.com>
2024-04-03 15:04:26 -0800

    Made normalizePriority better and faster

--- api/views/v1/kanban.py
+++ api/views/v1/kanban.py
@@ -141,6 +141,9 @@ class KanbanAPIView(APIView):
         Ensures all tasks have an integer priority value.
         Also ensures priority starts at 0 and is spaced evenly by 1.
 
+        Source for sorting list, pushing none values to end:
+        https://stackoverflow.com/questions/18411560/sort-list-while-pushing-none-values-to-the-end
+
         Args:
             tasks_by_status (dict): Dictionary containing tasks grouped by status.
 
@@ -149,18 +152,13 @@ class KanbanAPIView(APIView):
         """
         # Loop through status.
         for status_name, task_list in tasks_by_status.items():
-            # Initialize null priority.
-            for index, task in enumerate(task_list):
-                if task.priority is None:
-                    task.priority = index
-                    task.save()
-
-            sorted_tasks = sorted(task_list, key=lambda x: x.priority)
+            # Sort tasks with None pushed to the end
+            sorted_tasks = sorted(task_list, key=lambda x: (x['priority'] is None, x['priority']))
 
             # Space priority evenly.
             for index, task in enumerate(sorted_tasks):
-                if task.priority != index:
-                    task.priority = index
+                if task['priority'] != index:
+                    task['priority'] = index
                     task.save()
 
         return tasks_by_status
