cc69127f9fc453e563256629de9dd6f9f6347961
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-04-07 12:00:31 -0800

    pulled main

--- api/decorators.py
+++ api/decorators.py
@@ -9,8 +9,62 @@ from api.utils import decodeApiKey, decryptApiKey
 def apiKeyRequired(function):
     """
     Wraps function to authenticate it.
-    Allows token to be passed in the authorization header OR through cookies (header good for scripts,
+    Requires authorization token to be passed in the authorization header OR through cookies (header good for scripts,
     cookies good for websites).
+
+    @param function Function to be wrapped.
+    """
+
+    def wrap(request, *args, **kwargs):
+        token = request.META.get("HTTP_AUTHORIZATION")
+
+        # Try cookies if there is not a token in the header
+        if not token:
+            token = request.COOKIES.get("apiToken")
+
+        # If no token anywhere, raise an error
+        if not token:
+            return JsonResponse({"Error": "No token provided"}, status=401)
+
+        try:
+            decodedKey = decodeApiKey(token)
+        except jwt.ExpiredSignatureError:
+            return JsonResponse({"Error": "API Key is expired"}, status=401)
+        except jwt.InvalidTokenError:
+            return JsonResponse({"Error": "Invalid API Key"}, status=401)
+
+        # Verify that the user is in the database + API key matches (Can check perms if needed too)
+        userID = decodedKey.get("userID")
+        try:
+
+            user = User.objects.filter(id=userID).first()
+
+            if not user:
+                return JsonResponse(
+                    {"Error": "No user associated with that token"}, status=401
+                )
+
+            # Check API keys match
+            decryptedApiToken = decryptApiKey(user["apiKey"])
+            if token != decryptedApiToken:
+                return JsonResponse(
+                    {"Error": "No user associated with that token"}, status=401
+                )
+
+        except Exception:
+            return JsonResponse(
+                {"Error": "Could not find user associated with token"}, status=401
+            )
+        return function(request, *args, **kwargs)  # Call original function
+
+    return wrap
+
+
+def projectAuthorizationRequired(function):
+    """
+    Wraps project access functions to make sure user has authorization to access it.
+    Requires authorization token to be passed in the authorization header OR through cookies OR in the function params
+    (header good for scripts, cookies good for websites, function params good for internal function).
 
     @param function Function to be wrapped.
     """
