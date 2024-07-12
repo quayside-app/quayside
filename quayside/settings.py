6b9033748120d8445ed54f03b4cbac02ebc61448
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-07-12 00:10:56 -0800

    DB connection working but routes still buggy

--- quayside/settings.py
+++ quayside/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.getenv('GOOGLE_POSTGRES_HOST'),
-        'NAME': 'quayside-db',
+        'NAME': 'quayside-prod-db',
         'USER': os.getenv('GOOGLE_POSTGRES_USER'),
         'PASSWORD': os.getenv('GOOGLE_POSTGRES_PASSWORD'),
         
